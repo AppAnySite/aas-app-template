5965bb803199d198983dc187ec09f2c90742bee8
Vetagiri Hrushikesh <58148267+Vetagiri-Hrushikesh@users.noreply.github.com>
2025-09-05 20:44:34 +0530

    add component name

--- {{cookiecutter.project_name}}/index.js
+++ {{cookiecutter.project_name}}/index.js
@@ -4,6 +4,13 @@
 
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+console.log('[{{cookiecutter.project_name}}] entry loaded');
+AppRegistry.registerComponent('{{cookiecutter.project_name}}', () => App);
+
+// global error handler (safe guard)
+if (global.ErrorUtils) {
+  global.ErrorUtils.setGlobalHandler((error, isFatal) => {
+    console.error('[{{cookiecutter.project_name}}] global error:', error, 'isFatal:', isFatal);
+  });
+}
